f145c8641281d0ba76784fd823611fa27a1404bd
Chris Sonnier <christopher.sonnier@gmail.com>
2025-10-03 10:22:01 -0500

    fix: migration for adding tool to prompt for MySQL

--- db/migrate/20250930173443_add_tools_to_prompts.rb
+++ db/migrate/20250930173443_add_tools_to_prompts.rb
@@ -1,8 +1,8 @@
 class AddToolsToPrompts < ActiveRecord::Migration[8.0]
   def change
-    add_column :prompt_engine_prompts, :tools, :json, default: [], null: false
-    add_column :prompt_engine_prompt_versions, :tools, :json, default: [], null: false
-    add_index :prompt_engine_prompts, :tools, using: :gin
-    add_index :prompt_engine_prompt_versions, :tools, using: :gin
+    add_column :prompt_engine_prompts, :tools, :json, null: false
+    add_column :prompt_engine_prompt_versions, :tools, :json, null: false
+    add_index :prompt_engine_prompts, :tools
+    add_index :prompt_engine_prompt_versions, :tools
   end
 end
